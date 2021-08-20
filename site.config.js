e9b650a84430d49b950dc78246a25d1be211b5f2
SakenTam <49632755+SakenTam@users.noreply.github.com>
2021-08-20 17:17:41 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Second Blog',
+  domain: 'blog.sakentam.icu',
+  author: 'Saken Tam',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'This is Acting',
+  socialImageTitle: 'SakenTam',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'sakentamm',
+  github: 'sakentam',
+  linkedin: 'sakentam',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
